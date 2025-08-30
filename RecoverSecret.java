531c52ef3feef2179eee36bce8e727dfe4e7c74a
Ayyappan M P <131690195+Ayyappan-M-P@users.noreply.github.com>
2025-08-30 14:54:40 +0530

    Update RecoverSecret.java

--- RecoverSecret.java
+++ RecoverSecret.java
@@ -7,7 +7,7 @@ import java.util.*;
 /**
  * RecoverSecre
  * Reads JSON from input.json. Finds secret (f(0)) using Lagrange interpolation on k sized subsets,
- * picks the polynomial that matches the most shares, prints the secret and the wrong share keys.
+ * picks the polynomial that matche the most shares, prints the secret and the wrong share keys.
  *
  * Output format:
  * Secret: <decimal-string>
@@ -204,6 +204,7 @@ public class RecoverSecret {
         }
     }
 }
+
 
 
 
